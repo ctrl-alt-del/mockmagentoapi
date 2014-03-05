6b8ee03dd4cf7c32eda707d84f95856416552949
Jiyang Liu <jiyang881230@gmail.com>
2014-03-05 11:49:29 -0800

    grouping the routes into api/v1;

--- app/routes.php
+++ app/routes.php
@@ -18,22 +18,35 @@ Route::get('/', function()
 	return 'Welcome to Mock API';
 });
 
-Route::get('quotes', array('uses' => 'QuotesController@index'));
-Route::get('quotes/{id}', array('uses' => 'QuotesController@show'));
-Route::get('quotes/{id}/orders', array('uses' => 'QuotesController@showOrders'));
+Route::group(
+	array(
+		'prefix' 	=> 'api/v1',
+		'before' 	=> '',
+		'after' 	=> '',
+		),
+
+	function() {
+
+		Route::get('quotes', array('uses' => 'QuotesController@index'));
+		Route::get('quotes/{id}', array('uses' => 'QuotesController@show'));
+		Route::get('quotes/{id}/orders', array('uses' => 'QuotesController@showOrders'));
 
 
-Route::put('quotes/{id}', array('uses' => 'QuotesController@update'));
+		Route::put('quotes/{id}', array('uses' => 'QuotesController@update'));
 
-Route::get('products', array('uses' => 'ProductsController@index'));
-Route::get('products/{id}', array('uses' => 'ProductsController@show'));
-Route::get('products/sku/{sku}', array('uses' => 'ProductsController@showIdWithSku'));
-Route::put('products/{id}', array('uses' => 'ProductsController@update'));
+		Route::get('products', array('uses' => 'ProductsController@index'));
+		Route::get('products/{id}', array('uses' => 'ProductsController@show'));
+		Route::get('products/sku/{sku}', array('uses' => 'ProductsController@showIdWithSku'));
+		Route::put('products/{id}', array('uses' => 'ProductsController@update'));
 
 
-Route::get('orders', array('uses' => 'OrdersController@index'));
-Route::post('orders', array('uses' => 'OrdersController@store'));
-Route::delete('orders', array('uses' => 'OrdersController@destroyWithQuoteAndProductIds'));
+		Route::get('orders', array('uses' => 'OrdersController@index'));
+		Route::post('orders', array('uses' => 'OrdersController@store'));
+		Route::delete('orders', array('uses' => 'OrdersController@destroyWithQuoteAndProductIds'));
 
-Route::get('orders/{id}', array('uses' => 'OrdersController@show'));
-Route::put('orders/update', array('uses' => 'OrdersController@update'));
+		Route::get('orders/{id}', array('uses' => 'OrdersController@show'));
+		Route::put('orders/update', array('uses' => 'OrdersController@update'));
+
+
+	});
+
