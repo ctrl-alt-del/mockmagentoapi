b8a72932b9b149e6c3557087d1a956c4502b1af0
Jiyang Liu <jiyang881230@gmail.com>
2014-03-18 10:20:31 -0700

    update MaemController with new methods;

--- app/controllers/MaemController.php
+++ app/controllers/MaemController.php
@@ -5,20 +5,60 @@ class MaemController extends BaseController {
 	/**
 	 * Setup the layout used by the controller.
 	 *
-	 * @return void
+	 * @return hashset constains ids
 	 */
 	protected function pickRandomIds($table, $amount)
 	{
 		$hashset = array();
-		$totalRow = DB::table('orders')->count();
+		$totalRow = DB::table($table)->count();
 
-		while (count($hashset) < $amount) {
-			$hashset[rand(1, $totalRow)] = true;
-		}
+		do {
+			
+			$id = rand(1, $totalRow);
+
+			if (DB::table($table)->where('id', '=', $id) != null) {
+				$hashset[] = true;
+			}
+			
+		} while (count($hashset) < $amount);
 
 		return $hashset;
 	}
 
+	/**
+	 * Pick a random id of given table.
+	 *
+	 * @return id
+	 */
+	protected function pickRandomId($table) {
+
+		$totalRow = DB::table($table)->count();
+		
+		$id = rand(1, $totalRow);
+
+		if (DB::table($table)->where('id', '=', $id) == null) {
+			return $this->pickRandomId($table);
+		}
+		return $id;
+	}
+
+	/**
+	 * Pick a random id of given table and make sure it is not
+	 * existed in the given hashset
+	 *
+	 * @return id
+	 */
+	protected function pickAdditionalRandomIdForSet($table, $hashset) {
+
+		$totalRow = DB::table($table)->count();
+
+		do {
+			$id = rand(1, $totalRow);
+		} while (array_key_exists($id, $hashset) ||
+			DB::table($table)->where('id', '=', $id) == null);
+
+		return $id;
+	}
 
 
 }
